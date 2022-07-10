cff78999cd47426649e6801d489c59b7065973ad
Trevor Knott <Tknott95@hotmail.com>
2022-07-10 16:53:35 -0600

    testing process | adanodium v0.0.0

--- app/Main.hs
+++ app/Main.hs
@@ -8,5 +8,6 @@ main = do
   putStrLn "\n the Haskell API PAB replacement"
   i <- createProcess (proc "ls" [])
   j <- createProcess (proc "cardano-cli" ["--version"])
+  k <- createProcess (proc "cardano-cli" ["--", "query", "tip", "--testnet-magic", "1097911063"])
 
   putStrLn "\n Finished adanodium"
