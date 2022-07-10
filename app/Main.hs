65534e3584949d95d80f54c32636f8f4fd871635
Trevor Knott <Tknott95@hotmail.com>
2022-07-10 16:43:43 -0600

    adanodium v0.0.0

--- app/Main.hs
+++ app/Main.hs
@@ -1,5 +1,9 @@
+import System.Process
+
 module Main where
 
 main :: IO ()
 main = do
-  putStrLn "\n Haskell API PAB replacement"
+  putStrLn "\n the Haskell API PAB replacement"
+  r <- createProcess (proc "ls" [])
+  putStrLn "\n Finished adanodium"
