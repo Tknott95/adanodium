9e2ff687303197d2bd6f81ece4238a0adb5a09b9
Trevor Knott <Tknott95@hotmail.com>
2022-07-10 17:22:23 -0600

    testing process | adanodium v0.0.0

--- app/Main.hs
+++ app/Main.hs
@@ -3,6 +3,10 @@ module Main where
 import System.IO
 import System.Process
 
+-- @NOTES
+-- Pack flags with next item on list as flag val 
+-- into a large list of strings for params.
+
 
 main :: IO ()
 main = do
