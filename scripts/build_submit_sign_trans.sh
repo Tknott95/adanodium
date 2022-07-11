19447b195e3a185348007e2768b726f541f03ebf
Trevor Knott <tknott95@hotmail.com>
2022-07-11 17:22:26 -0600

    setting up haskell interaction | build_submit_trans.sh

--- scripts/build_submit_sign_trans.sh
+++ scripts/build_submit_sign_trans.sh
@@ -3,11 +3,14 @@
 sc_addr=$(cat ../keys/alwayssucceeds/alwayssucceeds_payment.addr)
 utxo_using=$(./grab_utxo.sh $sc_addr)"#0"
 
+# @TODO - make this dynamic - this is X4Wallet137
+payout_addr=addr_test1qpdvvdalsqscc3899gk67zdx7lkrlqlnwm3xzzk88jc65c50a06ns46p0wjxe6xqkvnrs4f79wjp6tz07wrl2k2nctyqqkhtak
+
 echo "SC_ADDR USING: " $sc_addr
 echo "UTXO USING:  " $utxo_using
 
-# ./build_trans.sh \
-#   $sc_addr\
-#   4ca42623c58dd0efc52790ab20c200a67ed3f70f3e273b800884093a0ea83678#0 \
-#   addr_test1qpdvvdalsqscc3899gk67zdx7lkrlqlnwm3xzzk88jc65c50a06ns46p0wjxe6xqkvnrs4f79wjp6tz07wrl2k2nctyqqkhtak \
-#   ../transactions/tx00/tx.build
+./build_trans.sh \
+  $sc_addr\
+  $utxo_using \
+  $payout_addr \
+  ../transactions/tx00/tx.build
