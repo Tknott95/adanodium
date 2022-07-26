7ec04a7110ecbe06f9020ae1b23d5c9d2a80a140
tknott95 <tknott95@hotmail.com>
2022-07-26 03:49:18 -0600

    added generics for the API the lib works in

--- app/models/TxModels.hs
+++ app/models/TxModels.hs
@@ -1,8 +1,12 @@
+{-# LANGUAGE DeriveGeneric #-}
+
 module TxModels where
+
+import GHC.Generics (Generic)
 
 data TXInfo = TXInfo {
    scriptAddr :: String,
    utxoUsing :: String,
    payoutAddr :: String,
    txOutLoc :: String 
-} deriving Show
+} deriving (Show, Generic)
